1ee7df9e7705a5f18a7cca987fca21259b96f0dd
walshg3 <walshg3@go.stockton.edu>
2022-02-27 15:32:34 -0500

    added HW2 and changed main.sh to work for hw 2

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@ cd /home/runner/CSCI-2101-S2022
 printf "Compiling..."
 # This line needs to be changed to whatever folder 
 # you are running code out of
-javac HW1/*.java 
+javac HW2/*.java 
 if [ $? == 0 ] 
 then
     clear
     # This line needs to be changed based off the name of the 
     # java file that has the main method
-    java HW1.Main
+    java HW2.MathGames
 else
     printf "\n\n\nError - Exit code $?"
 fi
