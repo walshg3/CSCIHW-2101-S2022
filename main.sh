b0e4616f48daed7447c1bb7302dab1a94a5cda33
walshg3 <walshg3@go.stockton.edu>
2022-03-27 16:40:32 -0400

    added HW3

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@ cd /home/runner/CSCIHW-2101-S2022
 printf "Compiling..."
 # This line needs to be changed to whatever folder 
 # you are running code out of
-javac Project1/*.java 
+javac HW3/*.java 
 if [ $? == 0 ] 
 then
     clear
     # This line needs to be changed based off the name of the 
     # java file that has the main method
-    java  Project1.BankAccountTester
+    java  HW3.Loops
 else
     printf "\n\n\nError - Exit code $?"
 fi
